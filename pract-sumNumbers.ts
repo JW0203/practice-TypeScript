0737fa887d4de5e194404d80e7f7d351f0bea929
JW0203 <widwoo0203@gmail.com>
2024-04-01 12:29:29 +0900

    practice sum all numbers in different ways

---
+++ pract-sumNumbers.ts
@@ -0,0 +1,44 @@
+const numbers = [90, 50, 80, 92, 82];
+const sumNumbers = (arr: number[]): number =>{
+    let result = 0;
+    for(let i of arr){
+        result += i;
+    }
+    return result;
+}
+console.log(sumNumbers(numbers))
+
+// reduce 이용
+const sumNumbersReduce = (arr: number[]):number =>{
+    // total 을 0 으로 초기화
+    return arr.reduce((total:number ,n:number) => total + n, 0);
+};
+console.log(sumNumbersReduce(numbers))
+
+
+// map 복습 및  이용
+const sumNumbersMap = (arr: number[]):number => {
+    let result = 0;
+    arr.map(number => {result += number;})
+    return result
+};
+console.log(sumNumbersMap(numbers))
+
+let result = 0;
+const sumNumbersMap2 = (arr:number[]):number =>{
+    arr.map((number,i) => result += arr[i]);
+    return result
+}
+console.log(sumNumbersMap2(numbers))
+
+
+// filter 복습 및  이용
+result = 0
+const sumNumbersFilter = (arr: number[]):number =>{
+    arr.filter((num, index)=>{
+        console.log(`${num} from index ${index}`)
+        result += num;
+    } )
+    return result;
+}
+console.log(sumNumbersFilter(numbers));
