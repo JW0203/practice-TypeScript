57a6a47a80a89a36ed4001086aad9226b1399ff4
JW0203 <widwoo0203@gmail.com>
2024-04-01 16:25:44 +0900

    make a function whether the input number is odd or even

---
+++ numberType.ts
@@ -0,0 +1,11 @@
+function oddOrEven(number : number) :void{
+    let result : string;
+    if (number %2 === 0){
+        result =  "짝수" // even
+    }
+    result = "홀수" // odd
+    return console.log(`${number}은 ${result} 입니다.`)
+}
+
+oddOrEven(17);
+oddOrEven(18)
