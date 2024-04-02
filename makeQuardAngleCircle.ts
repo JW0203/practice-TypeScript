5132cc859ba0e390f1800af5717fdbfe3ee53b04
JW0203 <widwoo0203@gmail.com>
2024-04-02 11:19:11 +0900

    feat: make a circle

--- makeQuardAngleCircle.ts
+++ makeQuardAngleCircle.ts
@@ -22,4 +22,26 @@ console.log(`가로 길이 : ${square.getWidth}`);
 console.log(`세로 길이 : ${square.getHeight}`);
 console.log(`넓이 : ${square.calculateArea()}`);
 
+class Circle{
+    pi:number = 3.14;
+    constructor(private _radius : number,){
+    }
 
+    get getRadius() {
+        return this._radius;
+    }
+
+    power(base:number, exponent:number):number{
+        // base : 밑 , exponent: 지수
+        return base**exponent
+    }
+
+    calculateArea():number{
+        return this.pi*Math.pow(this._radius,2);
+    }
+}
+
+console.log('---원의 넓이 구하기')
+const circle = new Circle(10);
+console.log(`반지름 길이 : ${circle.getRadius}`);
+console.log(`넓이 : ${circle.calculateArea()}`);
